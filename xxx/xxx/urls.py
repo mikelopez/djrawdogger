52b871b00f6f94eb1ec5f22e46a0688f8d3fa953
Mike Lopez <dev@scidentify.info>
2013-12-30 03:24:49 -0500

    urls change

--- xxx/xxx/urls.py
+++ xxx/xxx/urls.py
@@ -1,20 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 PROJECT_ROOTDIR = getattr(settings, "PROJECT_ROOTDIR", "")
-# Uncomment the next two lines to enable the admin:
+ENABLE_ADMIN = getattr(settings, "ENABLE_ADMIN", None)
+
+
 from django.contrib import admin
 admin.autodiscover()
 
-hidden_urls = getattr(settings, "hidden_urls", None)
-ENABLE_ADMIN = getattr(settings, "ENABLE_ADMIN", None)
 
-urlpatterns = patterns('',
-	(r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/media/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
-    (r'admin/', include(admin.site.urls)),
-    url(r'^$', 'xxx.www.views.index', name='home'),
-    (r'^(?P<path>[-\w]+)/(?P<filtername>[\w -]+)', 'xxx.www.views.index'),
-    (r'^(?P<path>[-\w]+)','xxx.www.views.index'),
-)
+
+
 if ENABLE_ADMIN:
 	urlpatterns = patterns('',
 		(r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/media/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
